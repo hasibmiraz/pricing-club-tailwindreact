9905c959ca7bdca079d9422bc393453dfe18818b
hasibmiraz <hasibmiraz09@gmail.com>
2022-03-29 23:10:06 +0600

    Responsive menu created

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '../Link/Link';
+import { MenuIcon, XIcon } from '@heroicons/react/solid';
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   const routes = [
     { id: 1, name: 'Home', link: '/home' },
     { id: 2, name: 'Shop', link: '/shop' },
@@ -10,8 +12,15 @@ const Navbar = () => {
     { id: 5, name: 'Contact', link: '/contact' },
   ];
   return (
-    <nav>
-      <ul className="md:flex justify-center">
+    <nav className="bg-indigo-200">
+      <div onClick={() => setOpen(!open)} className="h-6 w-6 md:hidden">
+        {open ? <XIcon /> : <MenuIcon />}
+      </div>
+      <ul
+        className={`md:flex justify-center absolute md:static bg-indigo-200 w-full duration-500 ease-in-out ${
+          open ? 'top-6' : 'top-[-120px]'
+        }`}
+      >
         {routes.map((route) => (
           <Link key={route.id} route={route} />
         ))}
